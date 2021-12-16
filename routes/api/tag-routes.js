27e981bdb014944fdfbeb4943ebb5fcc88f83254
Rae Alejandrino <raealejandrino@gmail.com>
2021-12-16 17:50:03 -0500

    Added route for tag update

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -60,6 +60,16 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
