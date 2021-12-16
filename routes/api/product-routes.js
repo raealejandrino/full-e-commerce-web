6a09eaa876ca37edba7df461cae93108de4356af
Rae Alejandrino <raealejandrino@gmail.com>
2021-12-16 00:53:05 -0500

    Seeded and tested get route, fixed tag include syntax for get products route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -17,7 +17,9 @@ router.get('/', (req, res) => {
       },
       {
         model: Tag,
-        attributes: ['id', 'tag_name']
+        attributes: ['id', 'tag_name'],
+        through: Product,
+        as: 'product_tags'
       }
     ]
   })
