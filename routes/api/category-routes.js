a53ad34b4bae1a61da35aa55a62cec0149b13030
Rae Alejandrino <raealejandrino@gmail.com>
2021-12-16 17:40:33 -0500

    Added put route for category updates

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -57,6 +57,16 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
