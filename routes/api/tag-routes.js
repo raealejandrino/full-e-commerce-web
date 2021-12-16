36815148198d8757c2b555a1c674cbe86d10a94f
Rae Alejandrino <raealejandrino@gmail.com>
2021-12-16 17:50:48 -0500

    Added delete route for tag removal

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -74,6 +74,16 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
