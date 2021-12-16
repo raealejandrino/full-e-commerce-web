e4e564fb30555e44ef1fe2ca6af5b05c5532cf9c
Rae Alejandrino <raealejandrino@gmail.com>
2021-12-16 00:55:28 -0500

    Added single product get route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -34,6 +34,30 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+
+  Product.findOne({
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name'],
+        through: Product,
+        as: 'product_tags'
+      }
+    ]
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // create new product
